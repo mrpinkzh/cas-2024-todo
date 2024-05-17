f79ae7ed881daff001d14c2be4eb9951f460f4d7
Manuel Markwalder <manuel.markwalder1@swisscom.com>
2024-05-17 14:00:01 +0200

    extended functionality

--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -1,4 +1,4 @@
-let model = { state: `INIT`, todos: [] }
+let model = { todoCreation: { state:'INIT' }, todos: [] }
 const root = document.querySelector('#main');
 const registeredEvents = [];
 const render = () => {
@@ -18,39 +18,68 @@ const render = () => {
     })
 }
 const updateModel = (updated) => {
-    model = updated;
+    model = {...model, ...updated};
     render();
 }
 
+const TodoCreation = () => ({
+    events: [
+        { selector: 'button#btnNew', ev: 'click', handler: () => updateModel({todoCreation: {state: 'CREATE'}}) },
+        { selector: 'button#btnCreate', ev: 'click', handler : (e) => {
+                const form = e.target.closest('form');
+                const title = form.querySelector('input[name="title"]').value
+                const importance = form.querySelector('input[name="importance"]').value
+                const dueDate = form.querySelector('input[name="dueDate"]').value
+                const description = form.querySelector('textarea[name="description"]').value
+                updateModel({ todoCreation: { state: 'INIT' }, todos: [...model.todos, {title, importance, dueDate, description}] })
+            }
+        }],
+    template: (model) => `
+        <div class="todocreation">
+            ${model.todoCreation.state === 'INIT'
+                ? ` <div class="todocreation__init">
+                        <button class="button" id="btnNew">New</button>
+                    </div>`
+                : ` <form>
+                        <div class="todocreation__create">
+                            <div class="form-input">
+                                <label for="title">title </label>
+                                <input class="form-input__textbox" type="text" name="title" maxlength="30"/>
+                            </div>
+                            <div class="form-input">
+                                <label for="importance">importance </label>
+                                <input class="form-input__textbox" type="number" name="importance"/>
+                            </div>
+                            <div class="form-input">
+                                <label for="dueDate">due date </label>
+                                <input class="form-input__textbox" type="date" name="dueDate" />
+                            </div>
+                            <div class="form-input">
+                                <label for="description">description </label>
+                                <textarea class="form-input__textbox" name="description"></textarea>
+                            </div>
+                            <div class="form-input">
+                                <button class="button" id="btnCreate">Create</button>
+                            </div>
+                        </div>
+                    </form>`
+            }
+        </div>`
+    })
+
 const App = () => {
     return ({
-        events: [
-            { selector: 'button#btnNew', ev: 'click', handler: (e) => updateModel({...model, state: 'CREATE'}) },
-            { selector: 'button#btnCreate', ev: 'click', handler : (e) => {
-                    const form = e.target.closest('form');
-                    const title = form.querySelector('input[name="title"]').value
-                    const importance = form.querySelector('input[name="importance"]').value
-                    updateModel({...model, state: 'INIT', todos: [...model.todos, {title, importance}]})
-                }
-            }],
+        events: [...TodoCreation().events],
         template: (model) => `
-            <div>
-                ${model.state === 'INIT'
-                    ? `<button class="button" id="btnNew">New</button>`
-                    : `
-                    <form>
-                        <label for="title">title: </label>
-                        <input type="text" name="title"/>
-                        <label for="importance">importance: </label>
-                        <input type="text" name="importance" />
-                        <button class="button" id="btnCreate">Create</button>
-                    </form>`
-                }
+            <div class="main">
+                ${TodoCreation().template(model)}
                 <div class="todolist">
                     ${model.todos.map(todo => `
                         <div class="todolist__todo">
                             <div>${todo.title}</div>
                             <div>${todo.importance}</div>
+                            <div>${todo.dueDate}</div>
+                            <div>${todo.description}</div>
                         </div>`)}
                 <div>
             </div>`
