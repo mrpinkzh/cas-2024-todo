57b2d71ae765b83922aaa371b73c68408fd0146d
Manuel Markwalder <manuel.markwalder@aias.ch>
2024-06-05 22:47:06 +0200

    introduced backend component

--- source/app.js
+++ source/app.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { getTodos, saveTodo } from './stores/todo-store.js';
 
 const app = express();
+const router = express.Router();
+
+router.get('/api/todos', async (req, res) => {
+    const todos = await getTodos();
+    res.json(todos)
+    res.sendStatus(200);
+})
+
+router.post('/api/todos', async (req, res) => {
+    await saveTodo(req.body);
+    res.sendStatus(200);
+})
 
 app.use(express.static('source/public'));
+app.use(bodyParser);
+app.use(router);
 
 export default app;
