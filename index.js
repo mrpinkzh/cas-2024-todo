4c7b2cd6168dea60e9f681f984a8035fdcbabb52
Manuel Markwalder <manuel.markwalder@aias.ch>
2024-06-05 22:08:41 +0200

    moved server app into own file.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import express from 'express';
+import app from './source/app.js'
 
-const app = express();
 const port = 3000;
 
-app.use(express.static('source/public'));
 
 app.listen(port, () => {
     // eslint-disable-next-line no-console
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Todo app listening at http://localhost:${port}`);
 });
